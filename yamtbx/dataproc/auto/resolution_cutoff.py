1a0423a545b1e3735921bf6a2442e347fc283011
Keitaro Yamashita <keitaroyam@users.noreply.github.com>
2016-10-05 20:36:06 +0900

    KAMO: more rigorous determination of high resolution cutoff based on CC1/2

--- yamtbx/dataproc/auto/resolution_cutoff.py
+++ yamtbx/dataproc/auto/resolution_cutoff.py
@@ -5,14 +5,139 @@ Author: Keitaro Yamashita
 This software is released under the new BSD License; see LICENSE.
 """
 
-# Original code from iotbx/merging_statistics.py
 
 import sys
 
 from libtbx import slots_getstate_setstate
 from iotbx import merging_statistics
+from libtbx.utils import null_out
+from libtbx import adopt_init_args
+
+import numpy
+import scipy.optimize
+
+class estimate_resolution_based_on_cc_half:
+  def __init__(self, i_obs, cc_half_min, cc_half_tol, n_bins, log_out=null_out()):
+    adopt_init_args(self, locals())
+    log_out.write("estimate_resolution_based_on_cc_half: cc_half_min=%.4f, cc_half_tol=%.4f n_bins=%d\n" % (cc_half_min, cc_half_tol, n_bins))
+    
+    self.d_min_data = i_obs.d_min()
+    self.d_min, self.cc_at_d_min = self.estimate_resolution()
+  # __init__()
+
+  @staticmethod
+  def fun_ed_aimless(s, d0, r):
+    # CC1/2 fitting equation used in Aimless, suggested by Ed Pozharski
+    return 0.5 * (1 - numpy.tanh((s-d0)/r))
+  # fun_ed_aimless()
+
+  def initial_est_byfit(self):
+    # Up to 200 bins. If few reflections, 50 reflections per bin. At least 9 shells.
+    n_bins = max(min(int(self.i_obs.size()/50. + .5), 200), 9)
+    self.log_out.write("Using %d bins for initial estimate\n" % n_bins)
+
+    self.i_obs.setup_binner(n_bins=n_bins)
+    bins = []
+
+    s_list, cc_list = [], []
+    for bin in self.i_obs.binner().range_used():
+      unmerged = self.i_obs.select(self.i_obs.binner().selection(bin))
+      try:
+        bin_stats = merging_statistics.merging_stats(unmerged,
+                                                     anomalous=False)
+        s_list.append(1./bin_stats.d_min**2)
+        cc_list.append(bin_stats.cc_one_half)
+      except RuntimeError: # complains that no reflections left after sigma-filtering.
+        continue
+
+    # Fit curve
+    def fun(x, s, cc):
+      d0, r = x
+      return self.fun_ed_aimless(s,d0,r) - cc
+    # fun()
+
+    x0 = [0.5*min(s_list), 1.] #Initial d0, r
+    self.log_out.write("  Initial d0, r = %s\n" % x0)
+    lsq = scipy.optimize.least_squares(fun, x0, args=(s_list, cc_list), loss="soft_l1", f_scale=.1)
+    #lsq = fit_ed(s_list, cc_list)
+
+    self.log_out.write("Least-square result:\n")
+    self.log_out.write(str(lsq))
+    self.log_out.write("\n")
+
+    d0, r = lsq.x
+    self.log_out.write("  Final d0, r = %s\n" % lsq.x)
+    d_min = 1./numpy.sqrt((numpy.arctanh(1.-2.*self.cc_half_min)*r + d0))
+    return d_min
+  # initial_est_byfit()
+
+  def cc_outer_shell(self, d_min):
+    binner = self.i_obs.setup_binner(d_min=d_min, n_bins=self.n_bins)
+    bin_stats = merging_statistics.merging_stats(self.i_obs.select(binner.selection(binner.range_used()[-1])), anomalous=False)
+    return bin_stats.cc_one_half
+  # cc_outer_shell()
+
+  def estimate_resolution(self):
+    if self.i_obs.size() == 0:
+      self.log_out.write("No reflections.\n")
+      return None, None
+
+    d_min = float("%.2f"%self.initial_est_byfit())
+    if d_min < self.d_min_data:
+      self.log_out.write("Warning: Initial estimate is higher than the limit of data (%.4f A < %.4f A)\n" %(d_min, self.d_min_data))
+      d_min = self.d_min_data
+
+    cc = self.cc_outer_shell(d_min)
+    self.log_out.write("Initial estimate: %.4f A (CC1/2= %.4f)\n" %(d_min, cc))
+
+    if cc >= self.cc_half_min and abs(cc - self.cc_half_min) < self.cc_half_tol:
+      return d_min, cc
+
+    if cc > self.cc_half_min + self.cc_half_tol:
+      upper_bound = d_min
+      lower_bound = d_min
+
+      for i in xrange(1,30):
+        if d_min-.1*i <= self.d_min_data: break
+        lower_bound = d_min-.1*i
+        cc = self.cc_outer_shell(lower_bound)
+        self.log_out.write("  CC1/2= %.4f at %.4f A\n" %(cc, d_min-.1*i))
+        if cc < self.cc_half_min:
+          break
+    else:
+      lower_bound = d_min
+      for i in xrange(1,30):
+        upper_bound = d_min+.1*i
+        cc = self.cc_outer_shell(upper_bound)
+        self.log_out.write("  CC1/2= %.4f at %.4f A\n" %(cc, d_min+.1*i))
+        if cc >= self.cc_half_min:
+          break
+
+    self.log_out.write("  Lower, Upper bound: %.4f, %.4f\n" %(lower_bound, upper_bound))
+
+    lb, ub = lower_bound, upper_bound
+    tmp_last = None
+
+    while True:
+      tmp = float("%.2f"%((lb+ub)/2.))
+      if tmp_last is not None and tmp == tmp_last: return ub, cc
+      tmp_last = tmp
+
+      cc = self.cc_outer_shell(tmp)
+      self.log_out.write("  CC1/2= %.4f at %.4f A\n" %(cc, tmp))
+
+      if cc < self.cc_half_min:
+        lb = tmp
+      elif (cc - self.cc_half_min) > self.cc_half_tol:
+        ub = tmp
+      else:
+        return tmp, cc
+  # estimate_resolution_based_on_cc_half()    
+
+
 
 class estimate_crude_resolution_cutoffs (slots_getstate_setstate) :
+  # Original code from iotbx/merging_statistics.py
   """
   Uses incredibly simplistic criteria to determine the approximate
   resolution limit of the data based on the merging statistics (using much
@@ -48,7 +173,7 @@ class estimate_crude_resolution_cutoffs (slots_getstate_setstate) :
     # Decide n_bins
     if n_bins is None:
         n_bins = min(200, int(i_obs.complete_set().indices().size()/500.+.5)) # not well tested.
-        #print "n_bins=",n_bins
+        print "n_bins=",n_bins
 
     i_obs.setup_binner(n_bins=n_bins)
     bins = []
