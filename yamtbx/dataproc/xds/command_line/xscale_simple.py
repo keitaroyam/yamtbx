8cfbeca427a9b698733c41c7daf142cc5638c7da
Keitaro Yamashita <keitaroyam@users.noreply.github.com>
2016-10-05 20:40:01 +0900

    xscale_simple.py: add lstin=, dmin_lst=, anomalous=, nproc, wfac1=, reference=, frames_per_batch=, corrections= options.

--- yamtbx/dataproc/xds/command_line/xscale_simple.py
+++ yamtbx/dataproc/xds/command_line/xscale_simple.py
@@ -7,6 +7,12 @@ This software is released under the new BSD License; see LICENSE.
 """
 
 master_params_str = """\
+lstin = None
+ .type = path
+dmin_lst = None
+ .type = path
+anomalous = False
+ .type = bool
 cell = average *first
  .type = choice(multi=False)
 sgnum = None
@@ -23,13 +29,28 @@ use_tmpdir_if_available = False
  .type = bool
 cbf_to_dat = True
  .type = bool
+nproc = None
+ .type = int
+
+wfac1 = None
+ .type = float
+reference = bmax bmed bmin
+ .type = choice(multi=False)
+ .help = Reference selection for final scaling
+frames_per_batch = None
+ .type = int
+ .help = affects NBATCH=. When 1, NBATCH= matches the number of frames in the dataset.
+corrections = decay modulation absorption
+ .type = choice(multi=True)
 """
 
 import os
 import sys
+import math
 import iotbx.phil
 from yamtbx import util
 from yamtbx.dataproc.xds import xscale
+from yamtbx.dataproc.xds.xds_ascii import XDS_ASCII
 
 xscale_comm = "xscale_par"
 
@@ -40,7 +61,7 @@ def check_valid_xac(xac):
     return "FORMAT=XDS_ASCII" in line
 # check_valid_xac()
 
-def get_xac_info(xac):
+def get_xac_info(xac, get_nframes=False):
     ret = {}
 
     for l in open(xac):
@@ -55,6 +76,10 @@ def get_xac_info(xac):
         elif l.startswith("!END_OF_HEADER"):
             break
 
+    if get_nframes:
+        frame_range = XDS_ASCII(f, read_data=False).get_frame_range()
+        ret["nframes"] = frame_range[1] - frame_range[0]
+
     return ret
 # get_xac_info()
 
@@ -66,19 +91,63 @@ def make_shells(d_max, d_min, nbins):
     return " RESOLUTION_SHELLS= %s\n" % rshells
 # make_shells()
 
+def prep_xscale_inp(wdir, xscale_inp_head, xac_files, infos, frames_per_batch=None, corrections=None, ref_idx=None):
+    xscale_inp = os.path.join(wdir, "XSCALE.INP")
+    inp_out = open(xscale_inp, "w")
+    inp_out.write(xscale_inp_head)
+
+    for i, xds_ascii in enumerate(xac_files):
+        star = "*" if ref_idx == i else " "
+        inp_out.write("  INPUT_FILE=%s%s\n" % (star, os.path.relpath(xds_ascii, params.workdir)))
+
+        if "resol_range_user" in infos[xds_ascii]:
+            inp_out.write("  INCLUDE_RESOLUTION_RANGE= %s\n" % infos[xds_ascii]["resol_range_user"])
+        else:
+            inp_out.write("  ! INCLUDE_RESOLUTION_RANGE= %s\n" % infos[xds_ascii]["resol_range"])
+
+        if frames_per_batch:
+            nbatch = int(math.ceil(infos[xds_ascii]["nframes"] / frames_per_batch))
+            inp_out.write("    NBATCH= %d\n" % nbatch)
+        if corrections:
+            inp_out.write("    CORRECTIONS= %s\n" % " ".join(map(lambda s: s.upper(), corrections)))
+
+        inp_out.write("\n")
+
+    inp_out.close()
+# prep_xscale_inp()
+
 def run(params, xac_files):
     if len(xac_files) == 0:
         print "No XDS_ASCII.HKL files provided."
         return
 
+    # Parse
+    dmin_dict = {}
+    if params.dmin_lst:
+        for l in open(params.dmin_lst):
+            sp = l.split()
+            if len(sp) != 2: continue
+            f, dmin = sp
+            dmin_dict[f] = dmin
+
     xscale_inp_head = "!MINIMUM_I/SIGMA= 3\n\n" 
+    if params.wfac1 is not None:
+        xscale_inp_head += "WFAC1= %.3f\n" % params.wfac1
+
+    if params.nproc:
+        xscale_inp_head += "MAXIMUM_NUMBER_OF_PROCESSORS= %d\n" % params.nproc
 
     infos = {}
     d_max, d_min = 0, 100
     cells = []
     for xds_ascii in xac_files:
-        info = get_xac_info(xds_ascii)
+        info = get_xac_info(xds_ascii, get_nframes=params.frames_per_batch is not None)
+        if xds_ascii in dmin_dict:
+            dmax,dmin = info["resol_range"].split()
+            info["resol_range_user"] = "%s %s" % (dmax, dmin_dict[xds_ascii])
+
         infos[xds_ascii] = info
+
 
         resrng = map(float, info["resol_range"].split())
         d_max = max(d_max, resrng[0])
@@ -97,26 +166,26 @@ def run(params, xac_files):
 
         xscale_inp_head += " SPACE_GROUP_NUMBER= %s\n" % sgnum
         xscale_inp_head += " UNIT_CELL_CONSTANTS= %s\n" % " ".join(map(lambda x: "%.3f"%x, cell_mean))
-
-    #if anomalous_flag is not None:
-    #    xscale_inp_head += " FRIEDEL'S_LAW= %s\n" % ("FALSE" if anomalous_flag else "TRUE")
-    
+  
     xscale_inp_head += make_shells(d_max, d_min, params.nbins) + "\n"
-    xscale_inp_head += " OUTPUT_FILE= %s\n\n" % params.output
+    xscale_inp_head += " OUTPUT_FILE= %s\n" % params.output
+    xscale_inp_head += "  FRIEDEL'S_LAW= %s\n\n" % ("FALSE" if params.anomalous else "TRUE")
 
-    xscale_inp = os.path.join(params.workdir, "XSCALE.INP")
-    inp_out = open(xscale_inp, "w")
-    inp_out.write(xscale_inp_head)
-
-    for xds_ascii in xac_files:
-        inp_out.write("  INPUT_FILE= %s\n" % os.path.relpath(xds_ascii, params.workdir))
-        inp_out.write("  ! INCLUDE_RESOLUTION_RANGE= %s\n\n" % infos[xds_ascii]["resol_range"])
-
-    inp_out.close()
+    prep_xscale_inp(params.workdir, xscale_inp_head, xac_files, infos, params.frames_per_batch, params.corrections)
 
     xscale.run_xscale(os.path.join(params.workdir, "XSCALE.INP"),
                       cbf_to_dat=params.cbf_to_dat,
                       use_tmpdir_if_available=params.use_tmpdir_if_available)
+
+    if params.reference:
+        print "Choosing reference data (reference=%s)" % params.reference
+        ref_idx = xscale.decide_scaling_reference_based_on_bfactor(os.path.join(params.workdir, "XSCALE.LP"), params.reference, return_as="index")
+        if ref_idx != 0:
+            for f in "XSCALE.INP", "XSCALE.LP": util.rotate_file(os.path.join(params.workdir, f))
+            prep_xscale_inp(params.workdir, xscale_inp_head, xac_files, infos, params.frames_per_batch, params.corrections, ref_idx=ref_idx)
+            xscale.run_xscale(os.path.join(params.workdir, "XSCALE.INP"),
+                              cbf_to_dat=params.cbf_to_dat,
+                              use_tmpdir_if_available=params.use_tmpdir_if_available)
 
 # run()
 
@@ -125,6 +194,8 @@ if __name__ == "__main__":
                                               master_string=master_params_str)
     params = cmdline.work.extract()
     xac_files = filter(check_valid_xac, cmdline.remaining_args)
+    if params.lstin:
+        xac_files.extend(filter(check_valid_xac, util.read_path_list(params.lstin)))
 
     print "XDS_ASCII.HKL files given:"
     for f in xac_files:
