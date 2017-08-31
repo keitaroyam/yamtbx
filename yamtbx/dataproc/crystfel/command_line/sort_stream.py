5e9da6d61a10cf751aa430560aa6ec5b126d5e17
Keitaro Yamashita <keitaroyam@users.noreply.github.com>
2017-08-31 21:07:16 +0900

    stop_after for crystfel.command_line.sort_stream SHA f0c11a2b

--- yamtbx/dataproc/crystfel/command_line/sort_stream.py
+++ yamtbx/dataproc/crystfel/command_line/sort_stream.py
@@ -1,7 +1,7 @@
 import cPickle as pickle
 import os
 
-def run(streamin, pklin, key, streamout=None):
+def run(streamin, pklin, key, stop_after=None, streamout=None):
     if streamout is None:
         streamout = os.path.splitext(os.path.basename(streamin))[0] + "_sort_%s.stream" % key
 
@@ -18,15 +18,16 @@ def run(streamin, pklin, key, streamout=None):
     ofs = open(streamout, "w")
     ofs.write(ifs.readline())
 
-    for idx in sorted_indices:
+    for i, idx in enumerate(sorted_indices):
         print "writing", stats[key][idx]
         s, e = stats["chunk_ranges"][idx]
         ifs.seek(s-1)
         ofs.write(ifs.read(e-s+1))
+        if stop_after is not None and i+1 >= stop_after: break
 
 # run()
 
 if __name__ == "__main__":
     import sys
-    run(sys.argv[1], sys.argv[2], sys.argv[3])
+    run(sys.argv[1], sys.argv[2], sys.argv[3], stop_after=int(sys.argv[4]) if len(sys.argv)>4 else None)
     
