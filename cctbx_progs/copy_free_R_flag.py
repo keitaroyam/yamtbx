6e7fcc7443b93c459dad5957f190c59a88ff951b
Keitaro Yamashita <keitaroyam@users.noreply.github.com>
2024-03-04 16:48:12 +0900

    copy_free_R_flag: bug fix

--- cctbx_progs/copy_free_R_flag.py
+++ cctbx_progs/copy_free_R_flag.py
@@ -46,10 +46,10 @@ def run(mtz, mtz_out, mtz_ref, flag_name=None, flag_value=None):
     else:
         flags = [x for x in ref_arrays if flag_name==x.info().label_string()]
         if len(flags) == 0:
-            print(" Specified flag name not found:", flag)
+            print(" Specified flag name not found:", flag_name)
             quit()
         else:
-            print(" Use specified flag:", flag)
+            print(" Use specified flag:", flag_name)
             flag_array = flags[0]
             
     # Get flag number
@@ -107,8 +107,8 @@ def run(mtz, mtz_out, mtz_ref, flag_name=None, flag_value=None):
     #mtz_dataset = create_mtz_dataset(miller_arrays)
     #mtz_dataset.add_miller_array(miller_array=r_free_flags, column_root_label=flag_name)
     #mtz_object = mtz_dataset.mtz_object()
-    mtz_object = iotbx.mtz.object(mtz).add_crystal(b"crystal", b"project", r_free_flags.unit_cell()). \
-        add_dataset(name=b"dataset", wavelength=0). \
+    mtz_object = iotbx.mtz.object(mtz).add_crystal("crystal", "project", r_free_flags.unit_cell()). \
+        add_dataset(name="dataset", wavelength=0). \
         add_miller_array(miller_array=r_free_flags, column_root_label=flag_name).mtz_object()
     #mtz_object.add_history("copy and extend test flag from "+mtz_ref)
     #mtz_object.show_summary(out=sys.stdout, prefix="  ")
